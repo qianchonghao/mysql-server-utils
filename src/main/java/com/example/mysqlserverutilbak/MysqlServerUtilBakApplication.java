b35c3b22aab1d762f307c3a551a278fdc7354f07
chonghao.qianchong <chonghao.qianchong@alibaba-inc.com>
2022-08-12 11:30:00 +0800

    init

---
+++ src/main/java/com/example/mysqlserverutilbak/MysqlServerUtilBakApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqlserverutilbak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlServerUtilBakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MysqlServerUtilBakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mysqlserverutilbak/MysqlServerUtilBakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqlserverutilbak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlServerUtilBakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
