41b91dcbb7c9dd5f0ecf0057d5d4acecf5fffd6b
chonghao.qianchong <chonghao.qianchong@alibaba-inc.com>
2022-08-12 12:07:28 +0800

    init

---
+++ src/main/java/com/example/mysqlserverutilbak/mysql/DBChecker.java
@@ -0,0 +1,22 @@
+package com.example.mysqlserverutilbak.mysql;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+/**
+ * @Author qch
+ * @Date 2022/8/12 11:58 上午
+ */
+@Component
+public class DBChecker implements InitializingBean {
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    private static final String SQL = "";
+
+
+
+}
---
+++ src/main/java/com/example/mysqlserverutilbak/mysql/util/SqlUtils.java
@@ -0,0 +1,74 @@
+package com.example.mysqlserverutilbak.mysql.util;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.function.Function;
+
+public class SqlUtils {
+    public static void executeUpdate(DataSource ds, String sql, Object... args) {
+        Connection conn = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            conn = ds.getConnection();
+            preparedStatement = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                preparedStatement.setObject(i, args[i]);
+            }
+            preparedStatement.execute();
+        } catch (SQLException e) {
+            System.out.println("sql execute fail");
+        } finally {
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("connection close fail");
+            }
+        }
+    }
+
+    public static <T> T executeQuery(DataSource ds, String sql, Function<ResultSet, T> func, Object... args) {
+        Connection conn = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            conn = ds.getConnection();
+            ps = conn.prepareStatement(sql);
+            for (int i = 0; i < args.length; i++) {
+                ps.setObject(i+1, args[i]);
+            }
+            rs = ps.executeQuery();
+            return func.apply(rs);
+        } catch (SQLException e) {
+            System.out.println("execute sql query fail");
+            return null;
+        } finally {
+            try {
+                if (ps != null) {
+                    ps.close();
+                }
+                if (conn != null) {
+                    conn.close();
+                }
+                if (rs != null) {
+                    rs.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("connection close fail");
+            }
+        }
+    }
+
+    public static ResultSet executeQuery(DataSource ds, String sql, Object... args) {
+        return executeQuery(ds, sql, (rs) -> {
+            return rs;
+        }, args);
+    }
+}
